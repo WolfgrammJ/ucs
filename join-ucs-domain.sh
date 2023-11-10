ad824a7154207c234b5b854a983c2c73ec8bca05
Jens Wolfgramm <Jens.Wolfgramm@damantec.de>
2023-11-10 04:39:32 +0100

    changes

--- join-ucs-domain.sh
+++ join-ucs-domain.sh
@@ -2,3 +2,5 @@ cd /tmp
 sudo wget "https://launchpad.net/~univention-dev/+archive/ubuntu/ppa/+files/python3-univention-domain-join_1.0-28ubuntu1_all.deb"
 sudo wget "https://launchpad.net/~univention-dev/+archive/ubuntu/ppa/+files/univention-domain-join-cli_1.0-28ubuntu1_all.deb"
 sudo wget "https://launchpad.net/~univention-dev/+archive/ubuntu/ppa/+files/univention-domain-join_1.0-28ubuntu1_all.deb"
+sudo apt update
+sudo apt install pyhton3-dnspython python3-ipy python3-ldap -y
