6d973033d010b1f0c12485d6bb3101bfb4fdcb88
Jens Wolfgramm <Jens.Wolfgramm@damantec.de>
2023-11-10 04:34:28 +0100

    changes

---
+++ join-ucs-domain.sh
@@ -0,0 +1,4 @@
+cd /tmp
+sudo wget "https://launchpad.net/~univention-dev/+archive/ubuntu/ppa/+files/python3-univention-domain-join_1.0-28ubuntu1_all.deb"
+sudo wget "https://launchpad.net/~univention-dev/+archive/ubuntu/ppa/+files/univention-domain-join-cli_1.0-28ubuntu1_all.deb"
+sudo wget "https://launchpad.net/~univention-dev/+archive/ubuntu/ppa/+files/univention-domain-join_1.0-28ubuntu1_all.deb"
