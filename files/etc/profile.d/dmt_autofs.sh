e8a67fba36dd0c93ff4ea408d345e3dd2950034c
Jens Wolfgramm <Jens.Wolfgramm@damantec.de>
2023-11-20 08:49:28 +0100

    changes

--- files/etc/profile.d/dmt_autofs.sh
+++ files/etc/profile.d/dmt_autofs.sh
@@ -21,3 +21,34 @@ if id -nG "$USER" | grep -qw "Domain Users"; then
   ln -s /mnt/user/$USER/Videos ~/Videos
   ln -s /mnt/user/$USER/Music ~/Music
 fi
+
+
+# Delete a possibly existing map file in users home before creating an new one
+rm -f ~/auto.damantec
+
+# check if user is member of group "g-share-damantec"
+if id -nG "$USER" | grep -qw "g-share-damantec"; then
+  # Add key to autofs map file in user's home directory (temporary)
+  echo "damantec -fstype=nfs4,rw 10.10.0.21:/volume1/share_damantec" >> ~/.auto.damantec
+fi
+
+# check if user is member of group "g-share-knowledge"
+if id -nG "$USER" | grep -qw "g-share-knowledge"; then
+  # Add key to autofs map file in user's home directory (temporary)
+  echo "knowledge -fstype=nfs4,rw 10.10.0.21:/volume1/share_knowledge" >> ~/.auto.damantec
+fi
+
+# check if user is member of group "g-share-development"
+if id -nG "$USER" | grep -qw "g-share-development"; then
+  # Add key to autofs map file in user's home directory (temporary)
+  echo "development -fstype=nfs4,rw 10.10.0.21:/volume1/share_development" >> ~/.auto.damantec
+fi
+
+# check if user is member of group "g-share-multimedia"
+if id -nG "$USER" | grep -qw "g-share-multimedia"; then
+  # Add key to autofs map file in user's home directory (temporary)
+  echo "multimedia -fstype=nfs4,rw 10.10.0.21:/volume1/share_multimedia" >> ~/.auto.damantec
+fi
+
+# Move file to final destination
+sudo mv -f ~/.auto.damantec /etc/auto.damantec
