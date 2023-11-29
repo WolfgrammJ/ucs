546b693424ddad0f36397d7b816438e92b539f9c
Jens Wolfgramm <Jens.Wolfgramm@damantec.de>
2023-11-29 09:39:01 +0100

    added Windows-Shares from dmtsdc001

--- files/etc/profile.d/w_dmt_autofs.sh
+++ files/etc/profile.d/w_dmt_autofs.sh
@@ -31,25 +31,47 @@ touch ~/.auto.damantec
 if id -nG "$USER" | grep -qw "g-share-damantec"; then
   # Add key to autofs map file in user's home directory (temporary)
   echo "damantec -fstype=nfs,vers=3,rw,async,noatime,nodiratime,relatime 10.10.0.21:/volume1/share_damantec" >> ~/.auto.damantec
+  echo "damantec_win -fstype=cifs,rw,noperm,vers=3.0,credentials=~/.smbcredentials ://dmtsdc001.ad.damantec.org/DAMANTEC" >> ~/.auto.damantec
+
 fi
 
 # check if user is member of group "g-share-knowledge"
 if id -nG "$USER" | grep -qw "g-share-knowledge"; then
   # Add key to autofs map file in user's home directory (temporary)
   echo "knowledge -fstype=nfs,vers=3,rw,async,noatime,nodiratime,relatime 10.10.0.21:/volume1/share_knowledge" >> ~/.auto.damantec
+  echo "knowledge_win -fstype=cifs,rw,noperm,vers=3.0,credentials=~/.smbcredentials ://dmtsdc001.ad.damantec.org/Knowledge" >> ~/.auto.damantec
 fi
 
 # check if user is member of group "g-share-development"
 if id -nG "$USER" | grep -qw "g-share-development"; then
   # Add key to autofs map file in user's home directory (temporary)
   echo "development -fstype=nfs,vers=3,rw,async,noatime,nodiratime,relatime 10.10.0.21:/volume1/share_development" >> ~/.auto.damantec
+  echo "development_win -fstype=cifs,rw,noperm,vers=3.0,credentials=~/.smbcredentials ://dmtsdc001.ad.damantec.org/Development" >> ~/.auto.damantec
 fi
 
 # check if user is member of group "g-share-multimedia"
 if id -nG "$USER" | grep -qw "g-share-multimedia"; then
   # Add key to autofs map file in user's home directory (temporary)
   echo "multimedia -fstype=nfs,vers=3,rw,async,noatime,nodiratime,relatime 10.10.0.21:/volume1/share_multimedia" >> ~/.auto.damantec
+  echo "multimedia_win -fstype=cifs,rw,noperm,vers=3.0,credentials=~/.smbcredentials ://dmtsdc001.ad.damantec.org/Multimedia" >> ~/.auto.damantec
 fi
+
+# check if user is member of group "g-share-software"
+if id -nG "$USER" | grep -qw "g-share-software"; then
+  # Add key to autofs map file in user's home directory (temporary)
+  echo "software -fstype=nfs,vers=3,rw,async,noatime,nodiratime,relatime 10.10.0.21:/volume1/share_software" >> ~/.auto.damantec
+  echo "software_win -fstype=cifs,rw,noperm,vers=3.0,credentials=~/.smbcredentials ://dmtsdc001.ad.damantec.org/Software" >> ~/.auto.damantec
+fi
+
+# check if user is member of group "g-share-temporaer"
+if id -nG "$USER" | grep -qw "g-share-temporaer"; then
+  # Add key to autofs map file in user's home directory (temporary)
+  echo "temporaer -fstype=nfs,vers=3,rw,async,noatime,nodiratime,relatime 10.10.0.21:/volume1/share_temporaer" >> ~/.auto.damantec
+  echo "temporaer_win -fstype=cifs,rw,noperm,vers=3.0,credentials=~/.smbcredentials ://dmtsdc001.ad.damantec.org/Temporaer" >> ~/.auto.damantec
+fi
+
+
+
 
 # check if map file was created, then move it to the final destination
 if [ -f ~/.auto.damantec ]; then
