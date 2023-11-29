2148aba1d708736af2f455194bc5d66641800a51
Jens Wolfgramm <Jens.Wolfgramm@damantec.de>
2023-11-29 10:15:54 +0100

    changes

--- files/etc/profile.d/w_dmt_autofs.sh
+++ files/etc/profile.d/w_dmt_autofs.sh
@@ -31,7 +31,7 @@ touch ~/.auto.damantec
 if id -nG "$USER" | grep -qw "g-share-damantec"; then
   # Add key to autofs map file in user's home directory (temporary)
   echo "damantec -fstype=nfs,vers=3,rw,async,noatime,nodiratime,relatime 10.10.0.21:/volume1/share_damantec" >> ~/.auto.damantec
-  echo "damantec_win -fstype=cifs,rw,noperm,vers=3.0,credentials=~/.smbcredentials ://dmtsdc001.ad.damantec.org/DAMANTEC" >> ~/.auto.damantec
+  echo "damantec_win -fstype=cifs,rw,noperm,vers=3.0,credentials=~/.smbcredentials ://10.10.0.1/DAMANTEC" >> ~/.auto.damantec
 
 fi
 
@@ -39,35 +39,35 @@ fi
 if id -nG "$USER" | grep -qw "g-share-knowledge"; then
   # Add key to autofs map file in user's home directory (temporary)
   echo "knowledge -fstype=nfs,vers=3,rw,async,noatime,nodiratime,relatime 10.10.0.21:/volume1/share_knowledge" >> ~/.auto.damantec
-  echo "knowledge_win -fstype=cifs,rw,noperm,vers=3.0,credentials=~/.smbcredentials ://dmtsdc001.ad.damantec.org/Knowledge" >> ~/.auto.damantec
+  echo "knowledge_win -fstype=cifs,rw,noperm,vers=3.0,credentials=~/.smbcredentials ://10.10.0.1/Knowledge" >> ~/.auto.damantec
 fi
 
 # check if user is member of group "g-share-development"
 if id -nG "$USER" | grep -qw "g-share-development"; then
   # Add key to autofs map file in user's home directory (temporary)
   echo "development -fstype=nfs,vers=3,rw,async,noatime,nodiratime,relatime 10.10.0.21:/volume1/share_development" >> ~/.auto.damantec
-  echo "development_win -fstype=cifs,rw,noperm,vers=3.0,credentials=~/.smbcredentials ://dmtsdc001.ad.damantec.org/Development" >> ~/.auto.damantec
+  echo "development_win -fstype=cifs,rw,noperm,vers=3.0,credentials=~/.smbcredentials ://10.10.0.1/Development" >> ~/.auto.damantec
 fi
 
 # check if user is member of group "g-share-multimedia"
 if id -nG "$USER" | grep -qw "g-share-multimedia"; then
   # Add key to autofs map file in user's home directory (temporary)
   echo "multimedia -fstype=nfs,vers=3,rw,async,noatime,nodiratime,relatime 10.10.0.21:/volume1/share_multimedia" >> ~/.auto.damantec
-  echo "multimedia_win -fstype=cifs,rw,noperm,vers=3.0,credentials=~/.smbcredentials ://dmtsdc001.ad.damantec.org/Multimedia" >> ~/.auto.damantec
+  echo "multimedia_win -fstype=cifs,rw,noperm,vers=3.0,credentials=~/.smbcredentials ://10.10.0.1/Multimedia" >> ~/.auto.damantec
 fi
 
 # check if user is member of group "g-share-software"
 if id -nG "$USER" | grep -qw "g-share-software"; then
   # Add key to autofs map file in user's home directory (temporary)
   echo "software -fstype=nfs,vers=3,rw,async,noatime,nodiratime,relatime 10.10.0.21:/volume1/share_software" >> ~/.auto.damantec
-  echo "software_win -fstype=cifs,rw,noperm,vers=3.0,credentials=~/.smbcredentials ://dmtsdc001.ad.damantec.org/Software" >> ~/.auto.damantec
+  echo "software_win -fstype=cifs,rw,noperm,vers=3.0,credentials=~/.smbcredentials ://10.10.0.1/Software" >> ~/.auto.damantec
 fi
 
 # check if user is member of group "g-share-temporaer"
 if id -nG "$USER" | grep -qw "g-share-temporaer"; then
   # Add key to autofs map file in user's home directory (temporary)
   echo "temporaer -fstype=nfs,vers=3,rw,async,noatime,nodiratime,relatime 10.10.0.21:/volume1/share_temporaer" >> ~/.auto.damantec
-  echo "temporaer_win -fstype=cifs,rw,noperm,vers=3.0,credentials=~/.smbcredentials ://dmtsdc001.ad.damantec.org/Temporaer" >> ~/.auto.damantec
+  echo "temporaer_win -fstype=cifs,rw,noperm,vers=3.0,credentials=~/.smbcredentials ://10.10.0.1/Temporaer" >> ~/.auto.damantec
 fi
 
 
@@ -75,9 +75,10 @@ fi
 if [ ! -f ~/.smbcredentials ]; then
   # Move file to final destination
   touch ~/.smbcredentials
-  echo "username=" >> ~./smbcredentials
-  echo "password=" >> ~./smbcredentials
-  echo "domain=ad.damantec.org" >> ~./smbcredentials
+  echo "username=" >> ~/.smbcredentials
+  echo "password=" >> ~/.smbcredentials
+  echo "domain=ad.damantec.org" >> ~/.smbcredentials
+  chmod 600 ~/.smbcredentials
 fi
 
 # check if map file was created, then move it to the final destination
